586fc7564e96297501db4294fc574d11bee56967
cstoneburner <cstoneburner@tradecrafted.com>
2015-10-06 11:27:11 -0700

    many changes, changed buttons again, cleaned up some selectors.

---
+++ js/wufoo_form.js
@@ -0,0 +1,16 @@
+var m1whb5621euyt9o;(function(d, t) {
+var s = d.createElement(t), options = {
+'userName':'womemingrowth',
+'formHash':'m1whb5621euyt9o',
+'autoResize':true,
+'height':'458',
+'async':true,
+'host':'wufoo.com',
+'header':'hide',
+'ssl':true};
+s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
+s.onload = s.onreadystatechange = function() {
+var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+try { m1whb5621euyt9o = new WufooForm();m1whb5621euyt9o.initialize(options);m1whb5621euyt9o.display(); } catch (e) {}};
+var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+})(document, 'script');
